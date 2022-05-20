a9e63c3f147c13b0132cb756e7d1aba934eeba89
chunnycode <chunnycode@gmail.com>
2022-05-21 03:03:18 +0900

    TokenProvider로 관심사 분리

---
+++ src/main/java/com/sample/shop/login/jwt/TokenProvider.java
@@ -0,0 +1,42 @@
+package com.sample.shop.login.jwt;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.util.Base64;
+import java.util.Date;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class TokenProvider {
+
+    @Value("${secret.access}")
+    private String secretKey;
+
+    private final long accessTokenValidTime = 1 * 60 * 1000L;
+
+    @PostConstruct
+    protected void init() {
+        secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
+    }
+
+    public String createToken(String memberName){
+        Claims claims = Jwts.claims();  // JWT payload 에 저장되는 정보단위
+        claims.put("memberName", memberName);
+        Date now = new Date();
+        return Jwts.builder()
+                .setClaims(claims) // 정보 저장
+                .setIssuedAt(now) // 토큰 발행 시간 정보
+                .setExpiration(new Date(now.getTime() + accessTokenValidTime)) // set Expire Time
+                .signWith(SignatureAlgorithm.HS256, secretKey)  // 사용할 암호화 알고리즘과
+                .compact();
+    }
+
+}
--- src/main/java/com/sample/shop/login/service/LoginServiceImpl.java
+++ src/main/java/com/sample/shop/login/service/LoginServiceImpl.java
@@ -2,6 +2,7 @@ package com.sample.shop.login.service;
 
 import com.sample.shop.login.dto.LoginDto;
 import com.sample.shop.login.entity.Member;
+import com.sample.shop.login.jwt.TokenProvider;
 import com.sample.shop.login.jwt.TokenResponse;
 import com.sample.shop.shared.repository.MemberRepository;
 import io.jsonwebtoken.Claims;
@@ -18,6 +19,7 @@ import java.util.Date;
 public class LoginServiceImpl implements LoginService{
 
     private final MemberRepository memberRepository;
+    private final TokenProvider tokenProvider;
 
     @Override
     @Transactional
@@ -30,17 +32,7 @@ public class LoginServiceImpl implements LoginService{
             throw new Exception("비밀번호가 일치하지 않습니다.");
         }
 
-        long ACCESS_TOKEN_VALID_TIME = 1 * 60 * 1000L;   // 1분
-        String SECRET_KEY = "Y2h1bm55Y29kZS1zcHJpbmctYm9vdC1qd3QtdHV0b3JpYWwtc2VjcmV0LWNodW5ueWNvZGUtdGVjaC1zcHJpbmctYm9vdC1qd3QtdHV0b3JpYWwtc2VjcmV0";
-        Claims claims = Jwts.claims();  // JWT payload 에 저장되는 정보단위
-        claims.put("memberId", member.getMemberId());
-        Date now = new Date();
-        String accessToken = Jwts.builder()
-                .setClaims(claims) // 정보 저장
-                .setIssuedAt(now) // 토큰 발행 시간 정보
-                .setExpiration(new Date(now.getTime() + ACCESS_TOKEN_VALID_TIME)) // set Expire Time
-                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)  // 사용할 암호화 알고리즘과
-                .compact();
+        String accessToken = tokenProvider.createToken(member.getMemberName());
 
         return TokenResponse.builder()
                 .ACCESS_TOKEN(accessToken)
