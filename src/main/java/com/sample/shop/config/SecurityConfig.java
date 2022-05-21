bc0a679120857feaeb1f0744a800c1dc57520662
chunnycode <chunnycode@gmail.com>
2022-05-21 18:19:18 +0900

    SecurityConfig 로그인 url 추가

--- src/main/java/com/sample/shop/config/SecurityConfig.java
+++ src/main/java/com/sample/shop/config/SecurityConfig.java
@@ -16,6 +16,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 						,"/favicon.ico"
 						,"/api/**"
 						,"/api/member/**"
+						,"/api/auth/**"
 						,"/member/**"
 				);
 	}
