85088ab348a3ef6f6e58d602174dc65953c7fa5e
chunnycode <chunnycode@gmail.com>
2022-05-25 19:47:31 +0900

    AOP Aspect 샘플 테스트 : 성공

---
+++ src/main/java/com/sample/shop/aopsample/SampleAspect.java
@@ -0,0 +1,32 @@
+package com.sample.shop.aopsample;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Slf4j
+public class SampleAspect {
+
+	@Pointcut("execution(* com.sample.shop.login.LoginController.*(..))")
+	private void doExecute() {}
+
+	@Around("doExecute()")
+	public Object doLogging(ProceedingJoinPoint joinPoint) throws Throwable{
+		log.info("In dologging");
+
+		String methodName = joinPoint.getSignature().toShortString();
+		try {
+			log.info(methodName+" is start");
+			Object obj = joinPoint.proceed();
+			return obj;
+		}finally {
+			log.info(methodName + " is Finish");
+		}
+	}
+
+}
