1133a79f198567b11129a4ac21924df74f705d75
chunnycode <chunnycode@gmail.com>
2022-06-14 23:52:49 +0900

    home 추가

--- src/main/java/com/sample/shop/web/member/MemberController.java
+++ src/main/java/com/sample/shop/web/member/MemberController.java
@@ -4,25 +4,23 @@ import com.sample.shop.domain.member.dto.MemberJoinDto;
 import com.sample.shop.domain.member.entity.Member;
 import com.sample.shop.domain.member.repository.MemberRepository;
 import com.sample.shop.domain.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
 @Slf4j
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/members")
 public class MemberController {
 
-	private final MemberService memberService;
+//	private final MemberService memberService;
 	private final MemberRepository memberRepository;
-
-	public MemberController(MemberService memberService, MemberRepository memberRepository) {
-		this.memberService = memberService;
-		this.memberRepository = memberRepository;
-	}
 
 	@GetMapping("/add")
 	public String addForm(@ModelAttribute("member") Member member){
@@ -30,7 +28,7 @@ public class MemberController {
 	}
 
 	@PostMapping("/add")
-	public String save(@Valid @ModelAttribute Member member, BindingResult bindingResult){
+	public String save(@Validated @ModelAttribute Member member, BindingResult bindingResult){
 		if(bindingResult.hasErrors()){
 			return "members/addMemberForm";
 		}
